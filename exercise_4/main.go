8b8e8304fba021317fc1d6907cd7264f0dfcbe52
Diego Urrego <diego.urrego@mercadolibre.com.co>
2024-01-02 16:27:46 -0500

    feat: exercise four completed.

---
+++ exercise_4/main.go
@@ -0,0 +1,45 @@
+package exercise_4
+
+import "math"
+
+func CalculateStatistics(operationType string) (func(nums ...int) int, string) {
+
+	switch operationType {
+	case "minimum":
+		return findMinimum, ""
+	case "average":
+		return calculateAverage, ""
+	case "maximum":
+		return findMaximum, ""
+	default:
+		return nil, ""
+	}
+}
+
+func findMaximum(nums ...int) int {
+	var maxNumber int
+	for _, num := range nums {
+		if num > maxNumber {
+			maxNumber = num
+		}
+	}
+	return maxNumber
+}
+
+func findMinimum(nums ...int) int {
+	var minNumber int = math.MaxInt
+	for _, num := range nums {
+		if num < minNumber {
+			minNumber = num
+		}
+	}
+	return minNumber
+}
+
+func calculateAverage(nums ...int) int {
+	var sum int
+	for _, num := range nums {
+		sum += num
+	}
+	return sum / len(nums)
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"functions_exercises/exercise_1"
 	"functions_exercises/exercise_2"
 	"functions_exercises/exercise_3"
+	"functions_exercises/exercise_4"
 )
 
 func main() {
@@ -12,14 +13,43 @@ func main() {
 	exercise_1.SalaryTaxes(150001)
 
 	// Exercise #2:
-	average, err := exercise_2.CalculateAverage(4.3, 2.1, 3.0)
+	averageOfNotes, err := exercise_2.CalculateAverage(4.3, 2.1, 3.0)
 	if err != "" {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("The average of notes is: %v\n", average)
+	fmt.Printf("The average of notes is: %v\n", averageOfNotes)
 
 	// Exercise #3:
 	employeeSalary := exercise_3.CalculateSalary(160, "C")
 	fmt.Printf("Tehe employee salary is: %v\n", employeeSalary)
+
+	const (
+		minimum = "minimum"
+		average = "average"
+		maximum = "maximum"
+	)
+
+	minFunc, err := exercise_4.CalculateStatistics(minimum)
+	if err != "" {
+		fmt.Println("ERROR. Please check the argument")
+	}
+
+	maxFunc, err := exercise_4.CalculateStatistics(maximum)
+	if err != "" {
+		fmt.Println("ERROR. Please check the argument")
+	}
+
+	averageFunc, err := exercise_4.CalculateStatistics(average)
+	if err != "" {
+		fmt.Println("ERROR. Please check the argument")
+	}
+
+	minValue := minFunc(2, 3, 3, 4, 10, 2, 4, 5)         // 2
+	averageValue := averageFunc(2, 3, 3, 4, 10, 2, 4, 5) // 10
+	maxValue := maxFunc(2, 3, 3, 4, 10, 2, 4, 5)         // 4
+
+	fmt.Printf("The min value is: %v, The max value is: %v and the average is: %v\n",
+		minValue, maxValue, averageValue)
+
 }

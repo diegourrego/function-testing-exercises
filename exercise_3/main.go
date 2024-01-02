86d2796fc4f609196356ef59b336fcace46cf6ee
Diego Urrego <diego.urrego@mercadolibre.com.co>
2024-01-02 12:46:49 -0500

    feat: Exercise three completed and added in main file.

---
+++ exercise_3/main.go
@@ -0,0 +1,24 @@
+package exercise_3
+
+import "fmt"
+
+func CalculateSalary(hoursPerMonth float32, category string) (salary float32) {
+	switch category {
+	case "C":
+		salary = 1000 * hoursPerMonth
+	case "B":
+		salary = (1500 * hoursPerMonth) + calculateMonthlyBonus(1500, 0.2)
+	case "A":
+		salary = (3000 * hoursPerMonth) + calculateMonthlyBonus(3000, 0.5)
+	default:
+		fmt.Println("Unexpected category. Check it and try again.")
+		salary = 0.0
+
+	}
+	return
+}
+
+func calculateMonthlyBonus(baseSalary float32, percentage float32) (salaryBonus float32) {
+	salaryBonus = (baseSalary * 8 * 30) * percentage
+	return
+}
--- main.go
+++ main.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 	"functions_exercises/exercise_1"
 	"functions_exercises/exercise_2"
+	"functions_exercises/exercise_3"
 )
 
 func main() {
+	// Exercise #1:
 	exercise_1.SalaryTaxes(150001)
+
+	// Exercise #2:
 	average, err := exercise_2.CalculateAverage(4.3, 2.1, 3.0)
 	if err != "" {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("The average of notes is: %v", average)
+	fmt.Printf("The average of notes is: %v\n", average)
+
+	// Exercise #3:
+	employeeSalary := exercise_3.CalculateSalary(160, "C")
+	fmt.Printf("Tehe employee salary is: %v\n", employeeSalary)
 }

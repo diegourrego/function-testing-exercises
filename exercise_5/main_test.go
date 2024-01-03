c15d6a91a7c6a1545640a5b7f0dd043016dffc6f
Diego Urrego <diego.urrego@mercadolibre.com.co>
2024-01-02 23:37:12 -0500

    feat: test file for exercise five added and unit tests implemented and running.

--- exercise_5/main_test.go
+++ exercise_5/main_test.go
@@ -1,1 +1,74 @@
-package exercise_5
+package exercise_5_test
+
+import (
+	"functions_exercises/exercise_5"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestDog(t *testing.T) {
+	// Arrange
+	mockAnimal := "dog"
+	var mockDogsQuantity uint8 = 4
+	expectedDogFood := float32(mockDogsQuantity) * 10.0
+
+	// Act
+	calculateDogFoodFunc, err := exercise_5.Animal(mockAnimal)
+	dogFoodObtained := calculateDogFoodFunc(mockDogsQuantity)
+	// Assert
+	require.Equal(t, expectedDogFood, dogFoodObtained)
+	require.Equal(t, "", err)
+}
+
+func TestCat(t *testing.T) {
+	// Arrange
+	mockAnimal := "cat"
+	var mockDogsQuantity uint8 = 7
+	expectedDogFood := float32(mockDogsQuantity) * 5.0
+
+	// Act
+	calculateDogFoodFunc, err := exercise_5.Animal(mockAnimal)
+	dogFoodObtained := calculateDogFoodFunc(mockDogsQuantity)
+	// Assert
+	require.Equal(t, expectedDogFood, dogFoodObtained)
+	require.Equal(t, "", err)
+}
+
+func TestHamster(t *testing.T) {
+	// Arrange
+	mockAnimal := "hamster"
+	var mockDogsQuantity uint8 = 12
+	expectedDogFood := float32(mockDogsQuantity) * 0.25
+
+	// Act
+	calculateDogFoodFunc, err := exercise_5.Animal(mockAnimal)
+	dogFoodObtained := calculateDogFoodFunc(mockDogsQuantity)
+	// Assert
+	require.Equal(t, expectedDogFood, dogFoodObtained)
+	require.Equal(t, "", err)
+}
+
+func TestTarantula(t *testing.T) {
+	// Arrange
+	mockAnimal := "tarantula"
+	var mockDogsQuantity uint8 = 2
+	expectedDogFood := float32(mockDogsQuantity) * 0.15
+
+	// Act
+	calculateDogFoodFunc, err := exercise_5.Animal(mockAnimal)
+	dogFoodObtained := calculateDogFoodFunc(mockDogsQuantity)
+	// Assert
+	require.Equal(t, expectedDogFood, dogFoodObtained)
+	require.Equal(t, "", err)
+}
+
+func TestAnimal_Error(t *testing.T) {
+	// Arrange
+	mockAnimal := "horse"
+
+	// Act
+	returnedFunction, err := exercise_5.Animal(mockAnimal)
+	// Assert
+	require.Nil(t, returnedFunction, "Should return a nil instead of a function")
+	require.Equal(t, "animal not found. check it and try again.", err)
+}
